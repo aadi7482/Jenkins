bf7227f37e9492ed4c8f0cbb0bb774fb32a3d7bd
Shivam Kumar Pandey <107880189+aadi7482@users.noreply.github.com>
2024-12-15 02:36:40 +0530

    Update Jenkins_installation_Centos-RHEL.sh

--- Jenkins_installation_Centos-RHEL.sh
+++ Jenkins_installation_Centos-RHEL.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+sudo yum update
+sudo yum install java-devel -y
+java --version
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo nano /etc/yum.repos.d/jenkins.repo
+[jenkins]
+name=Jenkins-stable
+baseurl=http://pkg.jenkins.io/redhat
+gpgcheck=1
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
